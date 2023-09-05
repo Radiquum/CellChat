4cddf67ae753510529ffd15cb28b671de5f2b334
Radiquum <kentai.waah@gmail.com>
2023-09-05 23:27:05 +0500

    fixed iri-uri encoding-decoding

--- cellchat/chat/views.py
+++ cellchat/chat/views.py
@@ -7,23 +7,24 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.urls import reverse
 from django.utils import timezone
 from .models import Message, Room
+from django.utils.encoding import uri_to_iri, iri_to_uri
+
 
 maxAge = 1 * 24 * 60 * 60 * 60
 
 def index(request):
     context = {}
     if request.COOKIES.get('room_id'):
-        context = {"last": request.COOKIES.get('room_id')}
+        context = {"last": uri_to_iri(request.COOKIES.get('room_id'))}
     return render(request, "index.html", context)
 
 def create(request):  # sourcery skip: use-named-expression
     if request.method == "GET":
         return render(request, "create.html")
     
-    request.encoding = ('utf-8')
-    room_id = request.POST.get('room_id').strip(" ")
-    username = request.POST.get('username')
-    password = request.POST.get('password').strip(" ")
+    room_id = iri_to_uri(request.POST.get('room_id').strip(" "))
+    username = iri_to_uri(request.POST.get('username'))
+    password = iri_to_uri(request.POST.get('password').strip(" "))
     
     if room_id == '':
         context = {
@@ -36,14 +37,14 @@ def create(request):  # sourcery skip: use-named-expression
         }
         return render(request, "create.html", context)
     
-    is_exist = Room.objects.filter(id=room_id).exists()
+    is_exist = Room.objects.filter(id=uri_to_iri(room_id)).exists()
     if is_exist: 
         context = {
             "error": "Комната существует!"
         }
         return render(request, "create.html", context) 
     
-    room = Room(id=room_id, password=password)
+    room = Room(id=uri_to_iri(room_id), password=uri_to_iri(password))
     room.save()
     response = redirect("room", room_id=room_id)
     response.set_cookie("room_id", room_id, max_age = maxAge)
@@ -55,8 +56,8 @@ def join(request):
     if request.method == "GET":
         return render(request, "join.html")
 
-    room_id = request.POST.get('room_id').strip(" ")
-    username = request.POST.get('username').strip(" ")
+    room_id = iri_to_uri(request.POST.get('room_id').strip(" "))
+    username = iri_to_uri(request.POST.get('username').strip(" "))
     
     if room_id == '':
         context = {
@@ -68,7 +69,7 @@ def join(request):
             "error": "Требуется имя!"
         }
         return render(request, "join.html", context)
-    room = Room.objects.filter(id=room_id)
+    room = Room.objects.filter(id=uri_to_iri(room_id))
     is_exist = room.exists()
     if not is_exist: 
         context = {
@@ -76,26 +77,25 @@ def join(request):
         }
         return render(request, "join.html", context) 
     
+    response = redirect("room", room_id=room_id)
     if room.get().password != '':
         response = redirect("password")
-        response.set_cookie("room_id", room_id, max_age = maxAge)
-        response.set_cookie("username", username, max_age = maxAge)
         return response
-    
-    response = redirect("room", room_id=room_id)
     response.set_cookie("room_id", room_id, max_age = maxAge)
     response.set_cookie("username", username, max_age = maxAge)
     return response
     
 
 def room(request, room_id, messages=5):
-    username = request.COOKIES.get('username')
-    password = request.COOKIES.get('password')
+    username = uri_to_iri(request.COOKIES.get('username'))
+    password = uri_to_iri(request.COOKIES.get('password'))
+    
     if password is None:
         password = ''
     if room_id is None or username is None:
         return redirect("index")
-    
+
+    room_id = uri_to_iri(room_id)
     room = Room.objects.filter(id=room_id)
     if password != room.get().password:
         return HttpResponseForbidden()
@@ -118,17 +118,17 @@ def room(request, room_id, messages=5):
 def password(request):
     if request.method == "GET":
         return render(request, "password.html")
-    room_id = request.COOKIES.get('room_id')
-    password = request.POST.get('password').strip(" ")
+    room_id = uri_to_iri(request.COOKIES.get('room_id'))
+    password = iri_to_uri(request.POST.get('password').strip(" "))
     room = Room.objects.filter(id=room_id)
 
-    if password != room.get().password:
+    if uri_to_iri(password) != room.get().password:
         context = {
             "error": "Неверный пароль!"
         }
         return render(request, "password.html", context)
     
-    response = redirect("room", room_id=room_id)
+    response = redirect("room", room_id=iri_to_uri(room_id))
     response.set_cookie("password", password, max_age = maxAge)
     return response
 
